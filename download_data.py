6d1aa195f540bf4e526d68fec8d4cfa7e24b1797
Vani Madupu <vanimadupum@gmail.com>
2025-07-23 16:38:32 +0530

    Initial commit - BeeBot Marigold Flower Detector

--- download_data.py
+++ download_data.py
@@ -1,5 +1,5 @@
 import roboflow
 
-rf = roboflow.Roboflow(api_key="YOUR_API_KEY")
-project = rf.workspace("YOUR_WORKSPACE").project("marigold")
+rf = roboflow.Roboflow(api_key="E1lW7SfMx7fHWUZKIg2J")
+project = rf.workspace("vmadupu2024").project("beebot-marigold-detector")
 dataset = project.version(5).download("yolov5")
